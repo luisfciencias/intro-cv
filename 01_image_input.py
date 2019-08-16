6a26a39a566cd2de10395f7a6834a7e1e0ed4e22
Luis Torres <luisfciencias@gmail.com>
2019-08-16 22:16:01 +1000

    ENHANCE: Display gray scale image

--- 01_image_input.py
+++ 01_image_input.py
@@ -1,14 +1,16 @@
 # skeleton for an image processing pipeline
 import cv2
+import matplotlib.pyplot as plt
 import sys
 
 path_to_image = sys.argv[1]
 
-img_array = cv2.imread(path_to_image)
-
+img_array = cv2.imread(path_to_image, 0)
 
 def function_to_do_something(img):
     print('Doing something with image ...')
-
+    print(img.shape)
+    plt.imshow(img, 'gray')
+    plt.show()
 
 function_to_do_something(img_array)
