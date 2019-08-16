7c741d19dad26621b8e7ea479887a03a4dc29f93
Luis Torres <luisfciencias@gmail.com>
2019-08-16 23:05:25 +1000

    FIX: PEP8 Conventions

--- 01_image_input.py
+++ 01_image_input.py
@@ -7,10 +7,12 @@ path_to_image = sys.argv[1]
 
 img_array = cv2.imread(path_to_image, 0)
 
+
 def function_to_do_something(img):
     print('Doing something with image ...')
     print(img.shape)
     plt.imshow(img, 'gray')
     plt.show()
+
 
 function_to_do_something(img_array)
